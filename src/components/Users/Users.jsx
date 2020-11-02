50ef1e59425ab494b4b30996b318abc24aed85f2
Kamran Aghayev <aghayev189@gmail.com>
2020-11-02 14:53:53 +0400

    follow-unfollow API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './Users.module.css';
 import userPhoto from '../../assets/images/user.png';
 import { NavLink } from 'react-router-dom';
+import * as axios from 'axios';
 
 let Users = (props) => {
 
@@ -29,8 +30,32 @@ let Users = (props) => {
             </div>
             <div>
                 {u.followed 
-                ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
-                : <button onClick={() => {props.follow(u.id)}}>Follow</button>}
+                ? <button onClick={() => {
+                    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                        withCredentials: true,
+                        headers: {
+                            'API-KEY': '7df7f75f-12fb-462a-96ee-f2a52eafee13'
+                        }
+                        })
+                        .then(response => {
+                        if(response.data.resultCode == 0){
+                            props.unfollow(u.id);
+                        }
+                        });
+                }}>Unfollow</button>
+                : <button onClick={() => {
+                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                    withCredentials: true,
+                    headers: {
+                        'API-KEY': '7df7f75f-12fb-462a-96ee-f2a52eafee13'
+                    }
+                    })
+                    .then(response => {
+                    if(response.data.resultCode == 0){
+                        props.follow(u.id);
+                    }
+                    }); 
+                   }}>Follow</button>}
             </div>
         </span>
         <span>
