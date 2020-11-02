4d39fd5152f260587748d544939ce5355b143d9e
Kamran Aghayev <aghayev189@gmail.com>
2020-11-02 10:58:41 +0400

    with Router, props.match.params

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
       <Navbar />
       <div className='app-wrapper-content'>
         <Route path='/dialogs' render={ () => <DialogsContainer /> } />
-        <Route path='/profile' render={ () => <ProfileContainer /> } />
+        <Route path='/profile/:userId?' render={ () => <ProfileContainer /> } />
         <Route path='/users' render={ () => <UsersContainer /> } />
       </div>
     </div>
